1f0c489261d53c4ae334df245649a2f9933eaa71
Edson Eddy <crew0eddy@gmail.com>
2022-12-08 23:20:32 -0400

    refactor

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import Map from 'ol/Map';
-import View from 'ol/View';
+import Map from "ol/Map";
+import View from "ol/View";
 import {
   Circle as CircleStyle,
   Fill,
@@ -7,51 +7,51 @@ import {
   Stroke,
   Style,
   Text,
-} from 'ol/style';
-import {Draw, Modify} from 'ol/interaction';
-import {LineString, Point} from 'ol/geom';
-import {OSM, Vector as VectorSource} from 'ol/source';
-import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
-import {getArea, getLength} from 'ol/sphere';
+} from "ol/style";
+import { Draw, Modify } from "ol/interaction";
+import { LineString, Point } from "ol/geom";
+import { OSM, Vector as VectorSource } from "ol/source";
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
+import { getArea, getLength } from "ol/sphere";
 
-import Graticule from 'ol/layer/Graticule';
-import TileDebug from 'ol/source/TileDebug';
-import proj4 from 'proj4';
-import {applyTransform} from 'ol/extent';
-import {get as getProjection, getTransform} from 'ol/proj';
-import {register} from 'ol/proj/proj4';
+import Graticule from "ol/layer/Graticule";
+import TileDebug from "ol/source/TileDebug";
+import proj4 from "proj4";
+import { applyTransform } from "ol/extent";
+import { get as getProjection, getTransform } from "ol/proj";
+import { register } from "ol/proj/proj4";
 
 const style = new Style({
   fill: new Fill({
-    color: 'rgba(255, 255, 255, 0.2)',
+    color: "rgba(255, 255, 255, 0.2)",
   }),
   stroke: new Stroke({
-    color: 'rgba(0, 0, 0, 0.5)',
+    color: "rgba(0, 0, 0, 0.5)",
     lineDash: [10, 10],
     width: 2,
   }),
   image: new CircleStyle({
     radius: 5,
     stroke: new Stroke({
-      color: 'rgba(0, 0, 0, 0.7)',
+      color: "rgba(0, 0, 0, 0.7)",
     }),
     fill: new Fill({
-      color: 'rgba(255, 255, 255, 0.2)',
+      color: "rgba(255, 255, 255, 0.2)",
     }),
   }),
 });
 
 const labelStyle = new Style({
   text: new Text({
-    font: '14px Calibri,sans-serif',
+    font: "14px Calibri,sans-serif",
     fill: new Fill({
-      color: 'rgba(255, 255, 255, 1)',
+      color: "rgba(255, 255, 255, 1)",
     }),
     backgroundFill: new Fill({
-      color: 'rgba(0, 0, 0, 0.7)',
+      color: "rgba(0, 0, 0, 0.7)",
     }),
     padding: [3, 3, 3, 3],
-    textBaseline: 'bottom',
+    textBaseline: "bottom",
     offsetY: -15,
   }),
   image: new RegularShape({
@@ -60,22 +60,22 @@ const labelStyle = new Style({
     angle: Math.PI,
     displacement: [0, 10],
     fill: new Fill({
-      color: 'rgba(0, 0, 0, 0.7)',
+      color: "rgba(0, 0, 0, 0.7)",
     }),
   }),
 });
 
 const tipStyle = new Style({
   text: new Text({
-    font: '12px Calibri,sans-serif',
+    font: "12px Calibri,sans-serif",
     fill: new Fill({
-      color: 'rgba(255, 255, 255, 1)',
+      color: "rgba(255, 255, 255, 1)",
     }),
     backgroundFill: new Fill({
-      color: 'rgba(0, 0, 0, 0.4)',
+      color: "rgba(0, 0, 0, 0.4)",
     }),
     padding: [2, 2, 2, 2],
-    textAlign: 'left',
+    textAlign: "left",
     offsetX: 15,
   }),
 });
@@ -84,38 +84,38 @@ const modifyStyle = new Style({
   image: new CircleStyle({
     radius: 5,
     stroke: new Stroke({
-      color: 'rgba(0, 0, 0, 0.7)',
+      color: "rgba(0, 0, 0, 0.7)",
     }),
     fill: new Fill({
-      color: 'rgba(0, 0, 0, 0.4)',
+      color: "rgba(0, 0, 0, 0.4)",
     }),
   }),
   text: new Text({
-    text: 'Drag to modify',
-    font: '12px Calibri,sans-serif',
+    text: "Drag to modify",
+    font: "12px Calibri,sans-serif",
     fill: new Fill({
-      color: 'rgba(255, 255, 255, 1)',
+      color: "rgba(255, 255, 255, 1)",
     }),
     backgroundFill: new Fill({
-      color: 'rgba(0, 0, 0, 0.7)',
+      color: "rgba(0, 0, 0, 0.7)",
     }),
     padding: [2, 2, 2, 2],
-    textAlign: 'left',
+    textAlign: "left",
     offsetX: 15,
   }),
 });
 
 const segmentStyle = new Style({
   text: new Text({
-    font: '12px Calibri,sans-serif',
+    font: "12px Calibri,sans-serif",
     fill: new Fill({
-      color: 'rgba(255, 255, 255, 1)',
+      color: "rgba(255, 255, 255, 1)",
     }),
     backgroundFill: new Fill({
-      color: 'rgba(0, 0, 0, 0.4)',
+      color: "rgba(0, 0, 0, 0.4)",
     }),
     padding: [2, 2, 2, 2],
-    textBaseline: 'bottom',
+    textBaseline: "bottom",
     offsetY: -12,
   }),
   image: new RegularShape({
@@ -124,7 +124,7 @@ const segmentStyle = new Style({
     angle: Math.PI,
     displacement: [0, 8],
     fill: new Fill({
-      color: 'rgba(0, 0, 0, 0.4)',
+      color: "rgba(0, 0, 0, 0.4)",
     }),
   }),
 });
@@ -133,12 +133,12 @@ const segmentStyles = [segmentStyle];
 
 const formatLength = function (line) {
   const length = getLength(line);
-  return '' + length.toFixed(2);
+  return length.toFixed(2);
 };
 
 const formatArea = function (polygon) {
   const area = getArea(polygon);
-  return '' + area.toFixed(2);
+  return "" + area.toFixed(2);
 };
 
 const osmSource = new OSM();
@@ -154,7 +154,7 @@ const debugLayer = new TileLayer({
 const graticule = new Graticule({
   // the style to use for the lines, optional.
   strokeStyle: new Stroke({
-    color: 'rgba(255,120,0,0.9)',
+    color: "rgba(255,120,0,0.9)",
     width: 2,
     lineDash: [0.5, 4],
   }),
@@ -162,7 +162,6 @@ const graticule = new Graticule({
   visible: false,
   wrapX: false,
 });
-
 
 const source = new VectorSource();
 
@@ -182,27 +181,27 @@ const map = new Map({
     graticule,
     vector,
   ],
-  target: 'map',
+  target: "map",
   view: new View({
-    projection: 'EPSG:3857',
+    projection: "EPSG:3857",
     center: [0, 0],
     zoom: 1,
   }),
 });
 
-const queryInput = document.getElementById('epsg-query');
-const searchButton = document.getElementById('epsg-search');
-const resultSpan = document.getElementById('epsg-result');
-const renderEdgesCheckbox = document.getElementById('render-edges');
-const showTilesCheckbox = document.getElementById('show-tiles');
-const showGraticuleCheckbox = document.getElementById('show-graticule');
+const queryInput = document.getElementById("epsg-query");
+const searchButton = document.getElementById("epsg-search");
+const resultSpan = document.getElementById("epsg-result");
+const renderEdgesCheckbox = document.getElementById("render-edges");
+const showTilesCheckbox = document.getElementById("show-tiles");
+const showGraticuleCheckbox = document.getElementById("show-graticule");
 
 function setProjection(code, name, proj4def, bbox) {
   if (code === null || name === null || proj4def === null || bbox === null) {
-    resultSpan.innerHTML = 'Nothing usable found, using EPSG:3857...';
+    resultSpan.innerHTML = "Nothing usable found, using EPSG:3857...";
     map.setView(
       new View({
-        projection: 'EPSG:3857',
+        projection: "EPSG:3857",
         center: [0, 0],
         zoom: 1,
       })
@@ -210,13 +209,13 @@ function setProjection(code, name, proj4def, bbox) {
     return;
   }
 
-  resultSpan.innerHTML = '(' + code + ') ' + name;
+  resultSpan.innerHTML = "(" + code + ") " + name;
 
-  const newProjCode = 'EPSG:' + code;
+  const newProjCode = "EPSG:" + code;
   proj4.defs(newProjCode, proj4def);
   register(proj4);
   const newProj = getProjection(newProjCode);
-  const fromLonLat = getTransform('EPSG:4326', newProj);
+  const fromLonLat = getTransform("EPSG:4326", newProj);
 
   let worldExtent = [bbox[1], bbox[2], bbox[3], bbox[0]];
   newProj.setWorldExtent(worldExtent);
@@ -236,21 +235,21 @@ function setProjection(code, name, proj4def, bbox) {
 }
 
 function search(query) {
-  resultSpan.innerHTML = 'Searching ...';
-  fetch('https://epsg.io/?format=json&q=' + query)
+  resultSpan.innerHTML = "Searching ...";
+  fetch("https://epsg.io/?format=json&q=" + query)
     .then(function (response) {
       return response.json();
     })
     .then(function (json) {
-      const results = json['results'];
+      const results = json["results"];
       if (results && results.length > 0) {
         for (let i = 0, ii = results.length; i < ii; i++) {
           const result = results[i];
           if (result) {
-            const code = result['code'];
-            const name = result['name'];
-            const proj4def = result['proj4'];
-            const bbox = result['bbox'];
+            const code = result["code"];
+            const name = result["name"];
+            const proj4def = result["proj4"];
+            const bbox = result["bbox"];
             if (
               code &&
               code.length > 0 &&
@@ -260,8 +259,14 @@ function search(query) {
               bbox.length == 4
             ) {
               setProjection(code, name, proj4def, bbox);
-              document.getElementById('epsg').innerHTML = map.getView().getProjection().getCode();
-              document.getElementById('units').innerHTML = map.getView().getProjection().getUnits();
+              document.getElementById("epsg").innerHTML = map
+                .getView()
+                .getProjection()
+                .getCode();
+              document.getElementById("units").innerHTML = map
+                .getView()
+                .getProjection()
+                .getUnits();
               return;
             }
           }
@@ -292,19 +297,19 @@ function onGraticuleChange() {
 function onTilesChange() {
   debugLayer.setVisible(showTilesCheckbox.checked);
 }
-showGraticuleCheckbox.addEventListener('change', onGraticuleChange);
-renderEdgesCheckbox.addEventListener('change', onReprojectionChange);
-showTilesCheckbox.addEventListener('change', onTilesChange);
+showGraticuleCheckbox.addEventListener("change", onGraticuleChange);
+renderEdgesCheckbox.addEventListener("change", onReprojectionChange);
+showTilesCheckbox.addEventListener("change", onTilesChange);
 
 onReprojectionChange();
 onGraticuleChange();
 onTilesChange();
 
-const typeSelect = document.getElementById('type');
-const showSegments = document.getElementById('segments');
-const clearPrevious = document.getElementById('clear');
+const typeSelect = document.getElementById("type");
+const showSegments = document.getElementById("segments");
+const clearPrevious = document.getElementById("clear");
 
-const modify = new Modify({source: source, style: modifyStyle});
+const modify = new Modify({ source: source, style: modifyStyle });
 
 let tipPoint;
 
@@ -314,11 +319,11 @@ function styleFunction(feature, segments, drawType, tip) {
   const type = geometry.getType();
   let point, label, line;
   if (!drawType || drawType === type) {
-    if (type === 'Polygon') {
+    if (type === "Polygon") {
       point = geometry.getInteriorPoint();
       label = formatArea(geometry);
       line = new LineString(geometry.getCoordinates()[0]);
-    } else if (type === 'LineString') {
+    } else if (type === "LineString") {
       point = new Point(geometry.getLastCoordinate());
       label = formatLength(geometry);
       line = geometry;
@@ -346,7 +351,7 @@ function styleFunction(feature, segments, drawType, tip) {
   }
   if (
     tip &&
-    type === 'Point' &&
+    type === "Point" &&
     !modify.getOverlay().getSource().getFeatures().length
   ) {
     tipPoint = geometry;
@@ -363,9 +368,9 @@ let draw; // global so we can remove it later
 function addInteraction() {
   const drawType = typeSelect.value;
   const activeTip =
-    'Click to continue drawing the ' +
-    (drawType === 'Polygon' ? 'polygon' : 'line');
-  const idleTip = 'Click to start measuring';
+    "Click to continue drawing the " +
+    (drawType === "Polygon" ? "polygon" : "line");
+  const idleTip = "Click to start measuring";
   let tip = idleTip;
   draw = new Draw({
     source: source,
@@ -374,17 +379,17 @@ function addInteraction() {
       return styleFunction(feature, showSegments.checked, drawType, tip);
     },
   });
-  draw.on('drawstart', function () {
+  draw.on("drawstart", function () {
     if (clearPrevious.checked) {
       source.clear();
     }
     modify.setActive(false);
     tip = activeTip;
   });
-  draw.on('drawend', function () {
+  draw.on("drawend", function () {
     modifyStyle.setGeometry(tipPoint);
     modify.setActive(true);
-    map.once('pointermove', function () {
+    map.once("pointermove", function () {
       modifyStyle.setGeometry();
     });
     tip = idleTip;
@@ -404,5 +409,3 @@ showSegments.onchange = function () {
   vector.changed();
   draw.getOverlay().changed();
 };
-
-
